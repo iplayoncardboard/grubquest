de4cded1f6e849c576f97eede48e4a6cef4f1436
Patrick Miles <patrick.miles@gmail.com>
2018-03-26 17:38:45 -0700

    saving before merge

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,8 +60,8 @@ function uberQuery(endEstLat, endEstLng){
     let fullAddress = street + " " + city + " " + state + " " + zip;
 
     
-    let adventureLevel = $("#stars:checked").val();
-    let priceNav = $("#dollars:checked").val(); 
+    var adventureLevel = $("#stars:checked").val();
+    var priceNav = $("#dollars:checked").val(); 
     
     console.log(adventureLevel);
     
@@ -182,24 +182,6 @@ function uberQuery(endEstLat, endEstLng){
           });
         };
 
-
-    
-    
-    //Get foursquare categories and search foursquare for items
-    async function getVenueDetails(venueID){
-        let venueToSearch = venueID;
-        let apiString= 'https://api.foursquare.com/v2/venues/'+venueToSearch+'?';
-        const clientID = '&client_id=P4KB5LUTWWYFAH4WWCI0OAA4UVU3NC0LKIKFJABAAAZ5ZBV0';
-        const clientSecret ='&client_secret=VPWEYY3QVF2CU10AKLACJPBIDYR4QIPG2PUUSBY30FZUITVJ';
-        const version = '&v=20170801';
-        let result = await $.ajax({
-            url: apiString+clientID+clientSecret+version,
-            method:'GET'
-        });
-        return result;
-        
-    }
-    
 
     
     var initialArray = createCategories(sessionStorage.getItem("adventureLevel"));    
